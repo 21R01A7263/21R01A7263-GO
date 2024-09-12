cf3123e8dbb73db46513a7224503e2772641dfb4
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2024-09-12 22:35:24 +0530

    Week3

--- Struct/father/father.go
+++ Struct/father/father.go
@@ -1,16 +1,15 @@
 package father
-
 import "fmt"
 
 func init() {
-    fmt.Println("Father package initialized")
+	fmt.Println("Father package initialized")
 }
 
 type Father struct {
-    Name string
+	Name string
 }
 
-func(f Father) Data(name string) string {
-    f.Name = "Father : " + name
-    return f.Name
+func (f Father) Data(name string) string {
+	f.Name = "Father : " + name
+	return f.Name
 }
