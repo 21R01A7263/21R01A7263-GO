3a129b62247f8cd2980d52b214ef16d9842f94f2
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2024-09-12 22:35:47 +0530

    Week3

--- Struct/father/son/son.go
+++ Struct/father/son/son.go
@@ -1,16 +1,15 @@
 package son
-
 import "fmt"
 
 func init() {
-    fmt.Println("Son package initialized")
+	fmt.Println("Son package initialized")
 }
 
 type Son struct {
-    Name string
+	Name string
 }
 
-func(s Son) Data(name string) string {
-    s.Name = "Son : " + name
-    return s.Name
+func (s Son) Data(name string) string {
+	s.Name = "Son : " + name
+	return s.Name
 }
