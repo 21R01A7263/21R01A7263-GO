4b86c56ef5bb47c0faea08d94a4a228f1780eb4b
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2024-09-13 10:42:43 +0530

    Week1

--- LCM_GCD.go
+++ LCM_GCD.go
@@ -1,45 +1,46 @@
 package main
 import "fmt"
 
-func lcm(t1 int, t2 int) {
-	var lcmnum int = 1
-	if t1 > t2 {
-		lcmnum = t1
-	} else {
-		lcmnum = t2
-	}
-	for {
-		if lcmnum%t1 == 0 && lcmnum%t2 == 0 {
-			fmt.Printf("LCM of %d and %d is %d", t1, t2, lcmnum)
-			break
-		}
-		lcmnum++
-	}
-	return
+func lcm(temp1 int,temp2 int) { 
+    var lcmnum int =1
+    if(temp1>temp2)    {
+        lcmnum=temp1
+    }else{
+        lcmnum=temp2
+    }
+    for {        
+        if(lcmnum%temp1==0 && lcmnum%temp2==0) {   
+            fmt.Printf("LCM of %d and %d is %d",temp1,temp2,lcmnum)            
+            break
+        }
+        lcmnum++
+    }
+    return
 }
 
-func gcd(t1 int, t2 int) {
-	var gcdnum int
-	for i := 1; i <= t1 && i <= t2; i++ {
-		if t1%i == 0 && gcdnum%i == 0 {
-			gcdnum = i
-		}
-	}
-	fmt.Print("gcd is:", gcdnum)
-	return
-}
+func gcd(temp1 int,temp2 int){
+    var gcdnum int
+    for i := 1; i <=temp1 && i <=temp2 ; i++ {
+            if(temp1%i==0 && temp2%i==0) {
+                gcdnum=i
+            } 
+        }
+        fmt.Printf("GCD of %d and %d is %d",temp1,temp2,gcdnum)
+    return
+}  
 
 func main() {
-	var n1, n2, action int
-	fmt.Println("enter 2 positive integers:")
-	fmt.Scanln(&n1)
-	fmt.Scanln(&n2)
-	fmt.Println("enter 1 for lcm and 2 for gcd")
-	fmt.Scanln(&action)
-	switch action {
-	case 1:
-		lcm(n1, n2)
-	case 2:
-		gcd(n1, n2)
-	}
+    var n1,n2,action int
+    fmt.Println("Enter two positive integers : ")
+    fmt.Scanln(&n1)
+    fmt.Scanln(&n2)
+
+    fmt.Println("Enter 1 for LCM and 2 for GCD")
+    fmt.Scanln(&action)
+    switch action {
+        case 1:
+            lcm(n1,n2)
+        case 2:
+            gcd(n1,n2)
+    }
 }
