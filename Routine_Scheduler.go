fc3c8856813451b98fc79abf1f65cf267e61691e
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2024-11-07 13:08:02 +0530

    Week14

--- Routine_Scheduler.go
+++ Routine_Scheduler.go
@@ -7,28 +7,24 @@ import (
 )
 
 func main() {
-
 	runtime.GOMAXPROCS(4)
-
 	var processTest sync.WaitGroup
 	processTest.Add(2)
-
 	go func() {
 		defer processTest.Done()
-		for i := 0; i < 3; i++ {
-			for j := 5; j <= 10; j++ {
+		for i := 0; i < 30; i++ {
+			for j := 15; j <= 25; j++ {
 				fmt.Printf(" %d", j)
-				if j == 7 {
+				if j == 18 {
 					fmt.Println()
 				}
 			}
 		}
 	}()
-
 	go func() {
 		defer processTest.Done()
 		for j := 0; j < 10; j++ {
-			for char := 'A'; char < 'A'+10; char++ {
+			for char := 'A'; char < 'A'+26; char++ {
 				fmt.Printf("%c ", char)
 				if char == 'F' {
 					fmt.Println()
